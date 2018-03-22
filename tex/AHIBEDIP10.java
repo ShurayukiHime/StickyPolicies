8ff079a258e66ba2ea31ea4b0745eb6768c2cd8f
ShurayukiHime <jos.7-11@hotmail.it>
2018-03-22 12:50:25 +0100

    1) finished explaining why not JPBC library 2) contestually added / modified needed java code to explain

--- tex/AHIBEDIP10.java
+++ tex/AHIBEDIP10.java
@@ -1,22 +1,14 @@
 // Setup
 AsymmetricCipherKeyPair keyPair = engine.setup(64, 3);
-
 // KeyGen
 Element[] ids = engine.map(keyPair.getPublic(), "angelo", "de caro", "unisa");
-
 CipherParameters sk0 = engine.keyGen(keyPair, ids[0]);
 CipherParameters sk01 = engine.keyGen(keyPair, ids[0], ids[1]);
 CipherParameters sk012 = engine.keyGen(keyPair, ids[0], ids[1], ids[2]);
-
-CipherParameters sk1 = engine.keyGen(keyPair, ids[1]);
-CipherParameters sk10 = engine.keyGen(keyPair, ids[1], ids[0]);
-CipherParameters sk021 = engine.keyGen(keyPair, ids[0], ids[2], ids[1]);
-
-// Encryption/Decryption
+// Encryption
 byte[][] ciphertext0 = engine.encaps(keyPair.getPublic(), ids[0]);
 byte[][] ciphertext01 = engine.encaps(keyPair.getPublic(), ids[0], ids[1]);
 byte[][] ciphertext012 = engine.encaps(keyPair.getPublic(), ids[0], ids[1], ids[2]);
-
 // Decrypt
 byte[] cleartext0 = engine.decaps(sk0, ciphertext0[1]);
 System.out.println(new String(cleartext0) + "");
